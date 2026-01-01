7b29cb3155fab343cfada9adf367857fd037d870
Eng Mushtaq <mushtaqhejaira@gmail.com>
2026-01-01 08:57:49 +0300

    last updates

--- fix_admin_constraint.sql
+++
@@ -1,7 +0,0 @@
--- Fix constraint to allow admin users
-ALTER TABLE users DROP CONSTRAINT IF EXISTS beekeeper_required_fields;
-
-ALTER TABLE users ADD CONSTRAINT beekeeper_required_fields CHECK (
-    user_type IN ('consumer', 'admin') OR 
-    (user_type = 'beekeeper' AND business_name IS NOT NULL)
-);
